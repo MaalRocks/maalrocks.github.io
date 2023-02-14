45baeba31d0d1224ff17491ac5d5e63f89afcaaa
maalrocks <spammersohn@gmail.com>
2023-02-14 05:40:57 +0100

    added tailwind

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss"
 export async function get() {
 	return rss({
 		title: "Rabbithole_AI | Blog",
-		description: "A Bunch",
+		description: "RSS Feed",
 		site: "https://maalrocks.github.io/",
 		items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
 		customData: `<language>de-DE</language>`,
