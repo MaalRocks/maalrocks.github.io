3295870b50cfe8123a731402e6270382aac1146f
maalrocks <spammersohn@gmail.com>
2023-04-02 01:54:15 +0200

    added navbar animation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import { Component, createSignal } from "solid-js"
+
+const Navbar: Component<{}> = (props) => {
+	const navPages = [
+		["Home", "/"],
+		["About", "/about/"],
+		["Blog", "/blog/"],
+		["Themen", "/tags/"],
+	]
+
+	const getInfos = () => {
+		console.log("getInfos func:")
+
+		const ul = document.getElementById("linkList")
+		ul?.classList.toggle("opacity-0")
+	}
+
+	return (
+		<>
+			<ul
+				id="linkList"
+				class="w-full flex justify-around items-center opacity-0 transition-opacity duration-500 ease-in-out"
+			>
+				{navPages.map(([title, url]) => (
+					<a
+						href={url}
+						class="rounded-lg font-medium text-fuchsia-100  hover:text-fuchsia-900 "
+					>
+						{title}
+					</a>
+				))}
+			</ul>
+
+			<button
+				class="cursor-pointer flex flex-col m-2"
+				onclick={() => {
+					getInfos()
+				}}
+			>
+				<span class="w-10 h-1.5 m-1 bg-fuchsia-600/60"></span>
+				<span class="w-10 h-1.5 mx-1 bg-fuchsia-600/60"></span>
+				<span class="w-10 h-1.5 m-1 bg-fuchsia-600/60"></span>
+			</button>
+		</>
+	)
+}
+
+export default Navbar
