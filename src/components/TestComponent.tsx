9f3318df6599ab9e83b3a7baf46d252a0c7e4543
maalrocks <spammersohn@gmail.com>
2023-04-01 23:42:25 +0200

    eines Tages: navBar

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -4,14 +4,18 @@ function TestComponent() {
 	const [count, setCount] = createSignal(0)
 
 	return (
-		<div className="mx-auto">
+		<div class="mx-auto">
 			<button
-				className="py-1 px-3 rounded-sm bg-blue-700"
+				class="py-1 px-3 rounded-sm bg-blue-700"
 				onClick={() => setCount(count() + 1)}
 			>
 				+1
 			</button>
-			<p>{count}</p>
+			<p>{count()}</p>
+
+			<button class="py-1 m-2 px-3 rounded-sm bg-blue-700">setCookie</button>
+
+			<button class="py-1 m-2 px-3 rounded-sm bg-blue-700">getCookie</button>
 		</div>
 	)
 }
