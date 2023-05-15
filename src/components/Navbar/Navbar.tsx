3516c1ec0a19271d276570dc2beb71a1f1267729
maalrocks <spammersohn@gmail.com>
2023-05-16 01:50:12 +0200

    anpassung

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -47,9 +47,20 @@ const Navbar: Component<{}> = () => {
 					setIsPrimary(!isPrimary())
 				}}
 			>
-				<span class="w-10 h-1.5 m-1 bg-fuchsia-600/60"></span>
-				<span class="w-10 h-1.5 mx-1 bg-fuchsia-600/60"></span>
-				<span class="w-10 h-1.5 m-1 bg-fuchsia-600/60"></span>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="h-12 opacity-80"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+					/>
+				</svg>
 			</button>
 		</>
 	)
